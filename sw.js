caee25fa0ee3b247de856cb921977a531893a927
NewYorkJanKees <huuspc@gmail.com>
2026-09-13 15:28:09 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="photopro-flashy-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
